1190652d6129f1c26df02495e489b1441c274cbe
Sergei Malafeev <sergeymalafeev@gmail.com>
2020-05-01 21:37:14 +0800

    #42 add support of env variables for auto exporter without OTA_EXPORTER_ prefix
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- auto-exporter/src/main/java/com/lightstep/opentelemetry/exporter/auto/LightstepSpanExporterFactory.java
+++ auto-exporter/src/main/java/com/lightstep/opentelemetry/exporter/auto/LightstepSpanExporterFactory.java
@@ -1,5 +1,6 @@
 package com.lightstep.opentelemetry.exporter.auto;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.lightstep.opentelemetry.exporter.LightstepConfig;
 import com.lightstep.opentelemetry.exporter.LightstepSpanExporter;
 import io.opentelemetry.sdk.contrib.auto.config.Config;
@@ -12,59 +13,142 @@ public class LightstepSpanExporterFactory implements SpanExporterFactory {
 
   @Override
   public SpanExporter fromConfig(Config config) {
-    final Properties properties = LightstepConfig
-        .loadConfig(config.getString(LightstepConfig.CONFIG_FILE, null));
+    final Properties properties = LightstepConfig.loadConfig(getConfigFile(config));
 
     try {
       return LightstepSpanExporter.newBuilder()
-          .setAccessToken(getProperty(properties, LightstepConfig.ACCESS_TOKEN_PROPERTY_KEY,
-              config.getString(LightstepConfig.ACCESS_TOKEN_PROPERTY_KEY, "")))
-          .setCollectorHost(getProperty(properties,
-              LightstepConfig.COLLECTOR_HOST_PROPERTY_KEY,
-              config.getString(LightstepConfig.COLLECTOR_HOST_PROPERTY_KEY,
-                  LightstepConfig.DEFAULT_HOST)))
-          .setCollectorPort(getProperty(properties, LightstepConfig.COLLECTOR_PORT_PROPERTY_KEY,
-              config.getInt(LightstepConfig.COLLECTOR_PORT_PROPERTY_KEY,
-                  LightstepConfig.DEFAULT_SECURE_PORT)))
-          .setCollectorProtocol(
-              getProperty(properties, LightstepConfig.COLLECTOR_PROTOCOL_PROPERTY_KEY,
-                  config.getString(LightstepConfig.COLLECTOR_PROTOCOL_PROPERTY_KEY,
-                      LightstepConfig.PROTOCOL_HTTPS)))
-          .setDeadlineMillis(getProperty(properties, LightstepConfig.DEADLINE_MILLIS_PROPERTY_KEY,
-              config.getLong(LightstepConfig.DEADLINE_MILLIS_PROPERTY_KEY,
-                  LightstepConfig.DEFAULT_DEADLINE_MILLIS)))
-          .setServiceName(getProperty(properties, LightstepConfig.SERVICE_NAME_PROPERTY_KEY,
-              config.getString(LightstepConfig.SERVICE_NAME_PROPERTY_KEY,
-                  config.getString(LightstepConfig.COMPONENT_NAME_PROPERTY_KEY,
-                      LightstepConfig.defaultServiceName()))))
-          .setServiceVersion(getProperty(properties,
-              LightstepConfig.SERVICE_VERSION_PROPERTY_KEY,
-              config.getString(LightstepConfig.SERVICE_VERSION_PROPERTY_KEY, null)))
+          .setAccessToken(getAccessToken(config, properties))
+          .setCollectorHost(getCollectorHost(config, properties))
+          .setCollectorPort(getCollectorPort(config, properties))
+          .setCollectorProtocol(getCollectorProtocol(config, properties))
+          .setDeadlineMillis(getDeadlineMillis(config, properties))
+          .setServiceName(getServiceName(config, properties))
+          .setServiceVersion(getServiceVersion(config, properties))
           .build();
     } catch (MalformedURLException e) {
       throw new RuntimeException(e);
     }
   }
 
-  private String getProperty(final Properties properties, final String key,
-      final String defaultValue) {
-    if (properties == null) {
-      return defaultValue;
-    }
-    return properties.getProperty(key, defaultValue);
+  @VisibleForTesting
+  static String getConfigFile(Config config) {
+    return config.getString(LightstepConfig.CONFIG_FILE_PROPERTY_KEY,
+        getPropertyFromSystem(LightstepConfig.CONFIG_FILE, String.class, null));
   }
 
-  private int getProperty(final Properties properties, final String key, final int defaultValue) {
-    if (properties == null) {
-      return defaultValue;
-    }
-    return Integer.parseInt(properties.getProperty(key, String.valueOf(defaultValue)));
+  @VisibleForTesting
+  static String getServiceVersion(Config config, Properties properties) {
+    return getProperty(config, properties,
+        LightstepConfig.SERVICE_VERSION_PROPERTY_KEY,
+        LightstepConfig.SERVICE_VERSION,
+        String.class,
+        null);
   }
 
-  private long getProperty(final Properties properties, final String key, final long defaultValue) {
+  @VisibleForTesting
+  static String getServiceName(Config config, Properties properties) {
+    return getProperty(config, properties, LightstepConfig.SERVICE_NAME_PROPERTY_KEY,
+        LightstepConfig.SERVICE_NAME,
+        String.class,
+        getProperty(config, properties, LightstepConfig.COMPONENT_NAME_PROPERTY_KEY,
+            LightstepConfig.COMPONENT_NAME,
+            String.class,
+            LightstepConfig.defaultServiceName()));
+  }
+
+  @VisibleForTesting
+  static String getAccessToken(Config config, Properties properties) {
+    return getProperty(config, properties, LightstepConfig.ACCESS_TOKEN_PROPERTY_KEY,
+        LightstepConfig.ACCESS_TOKEN, String.class, "");
+  }
+
+  @VisibleForTesting
+  static String getCollectorHost(Config config, Properties properties) {
+    return getProperty(config, properties, LightstepConfig.COLLECTOR_HOST_PROPERTY_KEY,
+        LightstepConfig.COLLECTOR_HOST,
+        String.class,
+        LightstepConfig.DEFAULT_HOST);
+  }
+
+  @VisibleForTesting
+  static int getCollectorPort(Config config, Properties properties) {
+    return getProperty(config, properties, LightstepConfig.COLLECTOR_PORT_PROPERTY_KEY,
+        LightstepConfig.COLLECTOR_PORT,
+        Integer.class,
+        LightstepConfig.DEFAULT_SECURE_PORT);
+  }
+
+  @VisibleForTesting
+  static String getCollectorProtocol(Config config, Properties properties) {
+    return getProperty(config, properties, LightstepConfig.COLLECTOR_PROTOCOL_PROPERTY_KEY,
+        LightstepConfig.COLLECTOR_PROTOCOL,
+        String.class,
+        LightstepConfig.PROTOCOL_HTTPS);
+  }
+
+  @VisibleForTesting
+  static long getDeadlineMillis(Config config, Properties properties) {
+    return getProperty(config, properties, LightstepConfig.DEADLINE_MILLIS_PROPERTY_KEY,
+        LightstepConfig.DEADLINE_MILLIS,
+        Long.class,
+        LightstepConfig.DEFAULT_DEADLINE_MILLIS);
+  }
+
+  private static <T> T getProperty(final Properties properties, final String key, Class<T> type) {
     if (properties == null) {
+      return null;
+    }
+    String val = properties.getProperty(key);
+    return convert(type, val);
+  }
+
+  @VisibleForTesting
+  static <T> T convert(Class<T> type, String val) {
+    if (val == null) {
+      return null;
+    }
+    final String typeSimpleName = type.getSimpleName();
+    switch (typeSimpleName) {
+      case "String":
+        return type.cast(val);
+      case "Integer":
+        return type.cast(Integer.parseInt(val));
+      case "Long":
+        return type.cast(Long.parseLong(val));
+      default:
+        throw new RuntimeException("Unsupported type " + typeSimpleName);
+    }
+  }
+
+  private static <T> T getProperty(Config config, Properties properties, String propKey,
+      String envKey, Class<T> type, T defaultValue) {
+
+    T fromFile = getProperty(properties, propKey, type);
+    if (fromFile != null) {
+      return fromFile;
+    }
+
+    T fromSystemOrDefault = getPropertyFromSystem(propKey, type,
+        getPropertyFromSystem(envKey, type, defaultValue));
+
+    final String typeSimpleName = type.getSimpleName();
+    switch (typeSimpleName) {
+      case "String":
+        return type.cast(config.getString(propKey, (String) fromSystemOrDefault));
+      case "Integer":
+        return type.cast(config.getInt(propKey, (Integer) fromSystemOrDefault));
+      case "Long":
+        return type.cast(config.getLong(propKey, (Long) fromSystemOrDefault));
+      default:
+        throw new RuntimeException("Unsupported type " + typeSimpleName);
+    }
+  }
+
+  private static <T> T getPropertyFromSystem(String name, Class<T> type, T defaultValue) {
+    String val = System.getProperty(name, System.getenv(name));
+    if (val == null || val.isEmpty()) {
       return defaultValue;
     }
-    return Long.parseLong(properties.getProperty(key, String.valueOf(defaultValue)));
+    return convert(type, val);
   }
 }
---
+++ auto-exporter/src/test/java/com/lightstep/opentelemetry/exporter/auto/LightstepSpanExporterFactoryTest.java
@@ -0,0 +1,364 @@
+package com.lightstep.opentelemetry.exporter.auto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.nullable;
+
+import com.lightstep.opentelemetry.exporter.LightstepConfig;
+import io.opentelemetry.sdk.contrib.auto.config.Config;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({LightstepSpanExporterFactory.class})
+public class LightstepSpanExporterFactoryTest {
+
+  private Config config;
+
+  @Before
+  public void before() {
+    config = Mockito.mock(Config.class);
+
+    Mockito.when(config.getString(anyString(), anyString())).thenAnswer(new Answer<String>() {
+      @Override
+      public String answer(InvocationOnMock invocationOnMock) {
+        return invocationOnMock.getArgument(1);
+      }
+    });
+
+    Mockito.when(config.getInt(anyString(), anyInt())).thenAnswer(new Answer<Integer>() {
+      @Override
+      public Integer answer(InvocationOnMock invocationOnMock) {
+        return invocationOnMock.getArgument(1);
+      }
+    });
+
+    Mockito.when(config.getLong(anyString(), anyLong())).thenAnswer(new Answer<Long>() {
+      @Override
+      public Long answer(InvocationOnMock invocationOnMock) {
+        return invocationOnMock.getArgument(1);
+      }
+    });
+  }
+
+  @After
+  public void after() {
+    System.clearProperty(LightstepConfig.ACCESS_TOKEN_PROPERTY_KEY);
+    System.clearProperty(LightstepConfig.COLLECTOR_HOST_PROPERTY_KEY);
+    System.clearProperty(LightstepConfig.COLLECTOR_PORT_PROPERTY_KEY);
+    System.clearProperty(LightstepConfig.COLLECTOR_PROTOCOL_PROPERTY_KEY);
+    System.clearProperty(LightstepConfig.DEADLINE_MILLIS_PROPERTY_KEY);
+    System.clearProperty(LightstepConfig.SERVICE_NAME_PROPERTY_KEY);
+    System.clearProperty(LightstepConfig.COMPONENT_NAME_PROPERTY_KEY);
+    System.clearProperty(LightstepConfig.SERVICE_VERSION_PROPERTY_KEY);
+    System.clearProperty(LightstepConfig.CONFIG_FILE_PROPERTY_KEY);
+  }
+
+  private void mockSystem() {
+    PowerMockito.mockStatic(System.class);
+    Mockito.when(System.getProperty(anyString(), anyString())).thenAnswer(
+        new Answer<String>() {
+          @Override
+          public String answer(InvocationOnMock invocationOnMock) {
+            return invocationOnMock.getArgument(1);
+          }
+        });
+  }
+
+  @Test
+  public void testConvert() {
+    assertEquals("test", LightstepSpanExporterFactory.convert(String.class, "test"));
+    assertEquals(123, (int) LightstepSpanExporterFactory.convert(Integer.class, "123"));
+    assertEquals(Long.MAX_VALUE,
+        (long) LightstepSpanExporterFactory.convert(Long.class, String.valueOf(Long.MAX_VALUE)));
+  }
+
+  @Test
+  public void testGetAccessToken_fromSystemProperty() {
+    System.setProperty(LightstepConfig.ACCESS_TOKEN_PROPERTY_KEY, "token-from-system-property");
+    final String accessToken = LightstepSpanExporterFactory.getAccessToken(config, null);
+    assertEquals("token-from-system-property", accessToken);
+  }
+
+  @Test
+  public void testGetAccessToken_fromConfig() {
+    Mockito.when(config.getString(LightstepConfig.ACCESS_TOKEN_PROPERTY_KEY, ""))
+        .thenReturn("token-from-config");
+    final String accessToken = LightstepSpanExporterFactory.getAccessToken(config, null);
+    assertEquals("token-from-config", accessToken);
+  }
+
+  @Test
+  public void testGetAccessToken_fromEnvVariable() {
+    mockSystem();
+    Mockito.when(System.getenv(LightstepConfig.ACCESS_TOKEN)).thenReturn("token-from-env-variable");
+    final String accessToken = LightstepSpanExporterFactory.getAccessToken(config, null);
+    assertEquals("token-from-env-variable", accessToken);
+  }
+
+  @Test
+  public void testGetCollectorPort_default() {
+    mockSystem();
+    final int port = LightstepSpanExporterFactory.getCollectorPort(config, null);
+    assertEquals(LightstepConfig.DEFAULT_SECURE_PORT, port);
+  }
+
+  @Test
+  public void testGetCollectorPort_fromEnvVariable() {
+    mockSystem();
+    Mockito.when(System.getenv(LightstepConfig.COLLECTOR_PORT)).thenReturn("123");
+    final int port = LightstepSpanExporterFactory.getCollectorPort(config, null);
+    assertEquals(123, port);
+  }
+
+  @Test
+  public void testGetCollectorPort_fromSystemProperty() {
+    System.setProperty(LightstepConfig.COLLECTOR_PORT_PROPERTY_KEY, "111");
+    final int port = LightstepSpanExporterFactory.getCollectorPort(config, null);
+    assertEquals(111, port);
+  }
+
+  @Test
+  public void testGetCollectorPort_fromConfig() {
+    Mockito.when(config
+        .getInt(LightstepConfig.COLLECTOR_PORT_PROPERTY_KEY, LightstepConfig.DEFAULT_SECURE_PORT))
+        .thenReturn(222);
+    final int port = LightstepSpanExporterFactory.getCollectorPort(config, null);
+    assertEquals(222, port);
+  }
+
+  @Test
+  public void testGetCollectorProtocol_default() {
+    mockSystem();
+    final String protocol = LightstepSpanExporterFactory.getCollectorProtocol(config, null);
+    assertEquals(LightstepConfig.PROTOCOL_HTTPS, protocol);
+  }
+
+  @Test
+  public void testGetCollectorProtocol_fromEnvVariable() {
+    mockSystem();
+    Mockito.when(System.getenv(LightstepConfig.COLLECTOR_PROTOCOL))
+        .thenReturn("protocol-from-env-variable");
+    final String protocol = LightstepSpanExporterFactory.getCollectorProtocol(config, null);
+    assertEquals("protocol-from-env-variable", protocol);
+  }
+
+  @Test
+  public void testGetCollectorProtocol_fromSystemProperty() {
+    System.setProperty(LightstepConfig.COLLECTOR_PROTOCOL_PROPERTY_KEY,
+        "protocol-from-system-property");
+    final String protocol = LightstepSpanExporterFactory.getCollectorProtocol(config, null);
+    assertEquals("protocol-from-system-property", protocol);
+  }
+
+  @Test
+  public void testGetCollectorProtocol_fromConfig() {
+    Mockito.when(config
+        .getString(LightstepConfig.COLLECTOR_PROTOCOL_PROPERTY_KEY, LightstepConfig.PROTOCOL_HTTPS))
+        .thenReturn("protocol-from-config");
+    final String protocol = LightstepSpanExporterFactory.getCollectorProtocol(config, null);
+    assertEquals("protocol-from-config", protocol);
+  }
+
+  @Test
+  public void testGetDeadlineMillis_default() {
+    mockSystem();
+    final long deadlineMillis = LightstepSpanExporterFactory.getDeadlineMillis(config, null);
+    assertEquals(LightstepConfig.DEFAULT_DEADLINE_MILLIS, deadlineMillis);
+  }
+
+  @Test
+  public void testGetDeadlineMillis_fromEnvVariable() {
+    mockSystem();
+    Mockito.when(System.getenv(LightstepConfig.DEADLINE_MILLIS)).thenReturn("123456789");
+    final long deadlineMillis = LightstepSpanExporterFactory.getDeadlineMillis(config, null);
+    assertEquals(123456789, deadlineMillis);
+  }
+
+  @Test
+  public void testGetDeadlineMillis_fromSystemProperty() {
+    System.setProperty(LightstepConfig.DEADLINE_MILLIS_PROPERTY_KEY, "1235");
+    final long deadlineMillis = LightstepSpanExporterFactory.getDeadlineMillis(config, null);
+    assertEquals(1235, deadlineMillis);
+  }
+
+  @Test
+  public void testGetDeadlineMillis_fromConfig() {
+    Mockito.when(config
+        .getLong(LightstepConfig.DEADLINE_MILLIS_PROPERTY_KEY,
+            LightstepConfig.DEFAULT_DEADLINE_MILLIS))
+        .thenReturn(333L);
+    final long deadlineMillis = LightstepSpanExporterFactory.getDeadlineMillis(config, null);
+    assertEquals(333, deadlineMillis);
+  }
+
+  @Test
+  public void testGetCollectorHost_default() {
+    mockSystem();
+    final String host = LightstepSpanExporterFactory.getCollectorHost(config, null);
+    assertEquals(LightstepConfig.DEFAULT_HOST, host);
+  }
+
+  @Test
+  public void testGetCollectorHost_fromEnvVariable() {
+    mockSystem();
+    Mockito.when(System.getenv(LightstepConfig.COLLECTOR_HOST))
+        .thenReturn("host-from-env-variable");
+    final String host = LightstepSpanExporterFactory.getCollectorHost(config, null);
+    assertEquals("host-from-env-variable", host);
+  }
+
+  @Test
+  public void testGetCollectorHost_fromSystemProperty() {
+    System.setProperty(LightstepConfig.COLLECTOR_HOST_PROPERTY_KEY,
+        "host-from-system-property");
+    final String host = LightstepSpanExporterFactory.getCollectorHost(config, null);
+    assertEquals("host-from-system-property", host);
+  }
+
+  @Test
+  public void testGetCollectorHost_fromConfig() {
+    Mockito.when(config
+        .getString(LightstepConfig.COLLECTOR_HOST_PROPERTY_KEY, LightstepConfig.DEFAULT_HOST))
+        .thenReturn("host-from-config");
+    final String host = LightstepSpanExporterFactory.getCollectorHost(config, null);
+    assertEquals("host-from-config", host);
+  }
+
+  @Test
+  public void testGetServiceName_default() {
+    mockSystem();
+    final String serviceName = LightstepSpanExporterFactory.getServiceName(config, null);
+    assertThat(serviceName).isNotBlank();
+  }
+
+  @Test
+  public void testGetServiceName_fromEnvVariable() {
+    mockSystem();
+    Mockito.when(System.getenv(LightstepConfig.SERVICE_NAME))
+        .thenReturn("service-from-env-variable");
+    final String serviceName = LightstepSpanExporterFactory.getServiceName(config, null);
+    assertEquals("service-from-env-variable", serviceName);
+  }
+
+  @Test
+  public void testGetServiceName_fromEnvVariable_componentName() {
+    mockSystem();
+    Mockito.when(System.getenv(LightstepConfig.COMPONENT_NAME))
+        .thenReturn("component-from-env-variable");
+    final String serviceName = LightstepSpanExporterFactory.getServiceName(config, null);
+    assertEquals("component-from-env-variable", serviceName);
+  }
+
+  @Test
+  public void testGetServiceName_fromSystemProperty() {
+    System.setProperty(LightstepConfig.SERVICE_NAME_PROPERTY_KEY,
+        "service-from-system-property");
+    final String serviceName = LightstepSpanExporterFactory.getServiceName(config, null);
+    assertEquals("service-from-system-property", serviceName);
+  }
+
+  @Test
+  public void testGetServiceName_fromSystemProperty_componentName() {
+    System.setProperty(LightstepConfig.COMPONENT_NAME_PROPERTY_KEY,
+        "component-from-system-property");
+    final String serviceName = LightstepSpanExporterFactory.getServiceName(config, null);
+    assertEquals("component-from-system-property", serviceName);
+  }
+
+  @Test
+  public void testGetServiceName_fromConfig() {
+    Mockito.when(config
+        .getString(eq(LightstepConfig.SERVICE_NAME_PROPERTY_KEY), anyString()))
+        .thenReturn("service-from-config");
+    final String serviceName = LightstepSpanExporterFactory.getServiceName(config, null);
+    assertEquals("service-from-config", serviceName);
+  }
+
+  @Test
+  public void testGetServiceName_fromConfig_componentName() {
+    Mockito.when(config
+        .getString(eq(LightstepConfig.COMPONENT_NAME_PROPERTY_KEY), anyString()))
+        .thenReturn("component-from-config");
+    final String serviceName2 = LightstepSpanExporterFactory.getServiceName(config, null);
+    assertEquals("component-from-config", serviceName2);
+  }
+
+  @Test
+  public void testGetServiceVersion_default() {
+    mockSystem();
+    final String serviceVersion = LightstepSpanExporterFactory.getServiceVersion(config, null);
+    assertThat(serviceVersion).isNull();
+  }
+
+  @Test
+  public void testGetServiceVersion_fromEnvVariable() {
+    mockSystem();
+    Mockito.when(System.getenv(LightstepConfig.SERVICE_VERSION))
+        .thenReturn("version-from-env-variable");
+    final String serviceVersion = LightstepSpanExporterFactory.getServiceVersion(config, null);
+    assertEquals("version-from-env-variable", serviceVersion);
+  }
+
+  @Test
+  public void testGetServiceVersion_fromSystemProperty() {
+    System.setProperty(LightstepConfig.SERVICE_VERSION_PROPERTY_KEY,
+        "version-from-system-property");
+    final String serviceVersion = LightstepSpanExporterFactory.getServiceVersion(config, null);
+    assertEquals("version-from-system-property", serviceVersion);
+  }
+
+  @Test
+  public void testGetServiceVersion_fromConfig() {
+    Mockito.when(config
+        .getString(eq(LightstepConfig.SERVICE_VERSION_PROPERTY_KEY), nullable(String.class)))
+        .thenReturn("version-from-config");
+    final String serviceVersion = LightstepSpanExporterFactory.getServiceVersion(config, null);
+    assertEquals("version-from-config", serviceVersion);
+  }
+
+  @Test
+  public void testConfigFile_default() {
+    mockSystem();
+    final String configFile = LightstepSpanExporterFactory.getConfigFile(config);
+    assertThat(configFile).isNull();
+  }
+
+  @Test
+  public void testConfigFile_fromEnvVariable() {
+    mockSystem();
+    Mockito.when(System.getenv(LightstepConfig.CONFIG_FILE))
+        .thenReturn("file-from-env-variable");
+    final String configFile = LightstepSpanExporterFactory.getConfigFile(config);
+    assertEquals("file-from-env-variable", configFile);
+  }
+
+  @Test
+  public void testConfigFile_fromSystemProperty() {
+    System.setProperty(LightstepConfig.CONFIG_FILE,
+        "file-from-system-property");
+    final String configFile = LightstepSpanExporterFactory.getConfigFile(config);
+    assertEquals("file-from-system-property", configFile);
+  }
+
+  @Test
+  public void testConfigFile_fromConfig() {
+    Mockito.when(config
+        .getString(eq(LightstepConfig.CONFIG_FILE_PROPERTY_KEY), nullable(String.class)))
+        .thenReturn("file-from-config");
+    final String configFile = LightstepSpanExporterFactory.getConfigFile(config);
+    assertEquals("file-from-config", configFile);
+  }
+}
